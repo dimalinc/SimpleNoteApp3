3bb03722f751e98b17f3afb7c28bd7ecddd83f4f
Val Okafor <okaatech@yahoo.com>
2015-10-01 09:40:50 -0700

    Complete Part 1

---
+++ app/src/main/java/com/okason/simplenotepad/utilities/Constants.java
@@ -0,0 +1,24 @@
+package com.okason.simplenotepad.utilities;
+
+/**
+ * Created by Valentine on 9/28/2015.
+ */
+public class Constants {
+
+    public static final String NOTES_TABLE = "notes";
+
+    public static final String COLUMN_ID = "_id";
+    public final static String COLUMN_NAME = "name";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_MODIFIED_TIME = "modified_time";
+    public static final String COLUMN_CREATED_TIME = "created_time";
+
+
+    public static final String[] COLUMNS = {
+            Constants.COLUMN_ID,
+            Constants.COLUMN_TITLE,
+            Constants.COLUMN_CONTENT,
+            Constants.COLUMN_CREATED_TIME
+    };
+}
